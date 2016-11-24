f1d21572de9d1ceff1dc4529b66292d41cb59877
loris ceschin <loris.ceschin@he-arc.ch>
2016-11-24 12:12:18 +0100

    removed styles v21 (useless) and small bug correction

--- Vitrine/app/src/main/java/com/vitrine/vitrine/TabActivity.java
+++ Vitrine/app/src/main/java/com/vitrine/vitrine/TabActivity.java
@@ -1,24 +1,16 @@
 package com.vitrine.vitrine;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 import com.vitrine.vitrine.fragments.SubscribedVitrinesFragment;
 
