be22a6d5a10b6f4cb565413dec917faa89c06664
joaquim <joaquim.perez@he-arc.ch>
2016-11-24 12:12:15 +0100

    Recadrage sur la vitrine au clic d'un cercle

--- HotPoints_Projects/TestLocation/app/src/main/java/vitrine/com/testmap/MapsActivity.java
+++ HotPoints_Projects/TestLocation/app/src/main/java/vitrine/com/testmap/MapsActivity.java
@@ -1,46 +1,30 @@
 package vitrine.com.testmap;
 
-import android.*;
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
-import android.net.Uri;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.HashMap;
 
@@ -52,11 +36,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
+
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
 
-    private HashMap<String,Vitrine> vitrines;
+    private HashMap<Circle,Vitrine> circlesVitrines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +53,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
@@ -79,6 +62,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .addApi(LocationServices.API)
                     .build();
         }
+
+
     }
 
     protected void onStart() {
@@ -104,14 +89,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Neuchatel and move the camera
-        LatLng neuchatel = new LatLng(46.989351, 6.929884);
-        //mMap.addMarker(new MarkerOptions().position(neuchatel).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(neuchatel));
-        //mMap.setMyLocationEnabled(true);
 
-        vitrines = new HashMap<>();
-
+        circlesVitrines = new HashMap<>();
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -155,21 +134,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             for (Vitrine vitrine : VitrineLoader.VITRINES) {
                 addVitrineCircle(vitrine);
             }
-                    
-            addVitrineCircle("Gare",200, Color.RED, new LatLng(46.996914, 6.935760));
-            addVitrineCircle("HE-Arc",100, Color.BLUE, latlng);
-
 
             mMap.setOnCircleClickListener(new GoogleMap.OnCircleClickListener() {
                 @Override
                 public void onCircleClick(Circle circle) {
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(circle.getCenter(),16));
 
-                    Vitrine vitrine = vitrines.get(circle.getId());
+                    Vitrine vitrine = circlesVitrines.get(circle);
+
+                    double x = vitrine.getRadius();
+                    // fonction trouvée par LoggerPro
+                    float y = (float) (25.77*Math.pow(x,-0.09));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(circle.getCenter(),y));
 
                     Snackbar snackbar = Snackbar
                             .make(getWindow().getDecorView(),vitrine.getNom(), Snackbar.LENGTH_LONG)
-                            .setAction("Contribuer", new View.OnClickListener() {
+                            .setAction("Voir", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
                                     //todo
@@ -193,27 +172,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Get back the mutable Circle
         Circle circle = mMap.addCircle(circleOptions);
-        vitrines.put(circle.getId(),vitrine);
-    }
-
-    private void addVitrineCircle(String name, int radius, int color, LatLng latlng) {
-        Vitrine vitrine = new Vitrine(name,radius,latlng,color);
-
-        // Instantiates a new CircleOptions object and defines the center and radius
-        CircleOptions circleOptions = new CircleOptions();
-        circleOptions.center(latlng);
-        circleOptions.radius(radius);
-
-        circleOptions.fillColor(Color.argb(90,Color.red(color),Color.green(color),Color.blue(color)));
-        circleOptions.strokeColor(Color.argb(120,Color.red(color),Color.green(color),Color.blue(color)));
-        circleOptions.clickable(true);
-
-        // Get back the mutable Circle
-        Circle circle = mMap.addCircle(circleOptions);
-        vitrines.put(circle.getId(),vitrine);
-
-        //mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-        //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        circlesVitrines.put(circle,vitrine);
     }
 
     @Override
--- HotPoints_Projects/TestLocation/app/src/main/java/vitrine/com/testmap/VitrineLoader.java
+++ HotPoints_Projects/TestLocation/app/src/main/java/vitrine/com/testmap/VitrineLoader.java
@@ -14,8 +14,9 @@ public class VitrineLoader {
     public static ArrayList<Vitrine> VITRINES = new ArrayList<>();
 
     static {
-        VITRINES.add(new Vitrine("HE-Arc",100, new LatLng(46.997637, 6.938717), Color.BLUE));
-        VITRINES.add(new Vitrine("Gare",200, new LatLng(46.996914, 6.935760), Color.RED));
-        VITRINES.add(new Vitrine("Parc technologique St-Imier",400, new LatLng(47.154859, 7.002969), Color.YELLOW));
+        VITRINES.add(new Vitrine("HE-Arc",123, new LatLng(46.997637, 6.938717), Color.BLUE));
+        VITRINES.add(new Vitrine("Gare",212, new LatLng(46.996914, 6.935760), Color.RED));
+        VITRINES.add(new Vitrine("Parc technologique St-Imier",356, new LatLng(47.154859, 7.002969), Color.YELLOW));
+        VITRINES.add(new Vitrine("La Chaux-de-Fonds",1400, new LatLng(47.103189, 6.827200), Color.GREEN));
     }
 }
