41e4ef3881f7f11051544f21daef948a94b6d98c
joaquim <joaquim.perez@he-arc.ch>
2017-01-10 10:11:35 +0100

    image uploader test tool

--- www/uploadPicture.php
+++ www/uploadPicture.php
@@ -5,23 +5,23 @@
     $name = $_POST['name'];
     $vitrine_id = $_POST['vitrineId'];
 
-    $path = "uploads/$vitrineId/$name.png";
+    $path = "uploads/$vitrine_id/$name.jpg";
 
     $sql = "INSERT INTO picture (path) VALUES ('$path')";
 
     $response = $conn->query($sql);
     if($response){
-        if (!is_dir("uploads/$vitrineId/")) {
+        if (!is_dir("uploads/$vitrine_id/")) {
             // dir doesn't exist, make it
-            mkdir("uploads/$vitrineId/");
+            mkdir("uploads/$vitrine_id/");
         }
 
-    file_put_contents($path,base64_decode($image));
+    file_put_contents($path,base64_decode($image, true));
     echo "Successfully Uploaded";
     }
     else{
         echo "Error inserting picture in database";
     }
-    $reponse->closeCursor();
+    //$reponse->closeCursor();
 
 ?>
