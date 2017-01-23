f9d503776be1408865448e587f71a24d3148a129
joaquim <joaquim.perez@he-arc.ch>
2017-01-23 22:08:17 +0100

    commentaires dans VitrineInfoActivity

--- Vitrine/app/src/main/java/com/vitrine/vitrine/VitrineInfoActivity.java
+++ Vitrine/app/src/main/java/com/vitrine/vitrine/VitrineInfoActivity.java
@@ -15,8 +15,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.util.Objects;
-
 public class VitrineInfoActivity extends AppCompatActivity {
 
     private Vitrine mVitrine;
@@ -31,49 +29,64 @@ public class VitrineInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vitrine_info);
 
+        // récupère l'utilisateur et la vitrine depuis l'intent qui a appellé cette activité
         Intent i = getIntent();
         mVitrine = i.getParcelableExtra("vitrine");
         User user = i.getParcelableExtra("user");
 
+        // récupère les 3 boutons depuis le layout
         btn_show = (Button) findViewById(R.id.btn_show);
         btn_subscribe = (Button) findViewById(R.id.btn_subscribe);
         btn_unsubscribe = (Button) findViewById(R.id.btn_unsubscribe);
 
+        // set le nom de la vitrine comme titre de l'activité
         setTitle(mVitrine.getName());
 
+        // itinitialise l'objet Volley qui permet d'envoyer des requêtes http
         queue = Volley.newRequestQueue(this);
 
+        // crée la string des paramètres GET et l'inclue à l'url
         final String params = String.format(
                 "?token=%s&vitrine_id=%s",
                 user.getToken(),
                 mVitrine.getId());
-        String url = getString(R.string.is_subscribed) + params;
+        String is_subscribed_url = getString(R.string.is_subscribed) + params;
 
-        StringRequest stringRequest = new StringRequest(
-            Request.Method.GET,
-            url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    if (response.equals("0")){
-                        btn_subscribe.setVisibility(View.VISIBLE);
-                    }else if (response.equals("1")){
-                        btn_unsubscribe.setVisibility(View.VISIBLE);
-                    }
-                    dialog.dismiss();
-                }
-            },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(VitrineInfoActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        );
-
-        queue.add(stringRequest);
+        // affiche le loader
         dialog = ProgressDialog.show(this, "", "Loading. Please wait...", true);
 
+        // prépare la requête
+        StringRequest is_subscribed_reqest = new StringRequest(
+                Request.Method.GET,
+                is_subscribed_url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // le serveur retourne le nombre d'entrées dans la table subscribed
+                        // qui correspondent au couple user/vitrine
+                        if (response.equals("0")){
+                            // l'utilisateur n'est pas abonné à la vitrine, on lui propose de s'abonner
+                            btn_subscribe.setVisibility(View.VISIBLE);
+                        }else if (response.equals("1")){
+                            // l'utilisateur est abonné à la vitrine, on lui propose de se désabonner
+                            btn_unsubscribe.setVisibility(View.VISIBLE);
+                        }
+                        // cache le loader
+                        dialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(VitrineInfoActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        // exécute la requête puis affiche l'un des deux boutons subscribe ou unsubscribe
+        queue.add(is_subscribed_reqest);
+
+        // lors du clic sur show vitrine, appelle l'activité VitrineActivity qui affiche les photos dans cette vitrine
         btn_show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,6 +96,7 @@ public class VitrineInfoActivity extends AppCompatActivity {
             }
         });
 
+        // exécute la requête d'abonnement à la vitrine
         btn_subscribe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,6 +105,7 @@ public class VitrineInfoActivity extends AppCompatActivity {
             }
         });
 
+        // exécute la requête de désabonnement à la vitrine
         btn_unsubscribe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,6 +116,7 @@ public class VitrineInfoActivity extends AppCompatActivity {
 
     }
 
+    // exécute une requête http get, puis quitte l'activité et affiche le résultat de la requête dans un Toast
     private void execRequest(String url) {
         StringRequest req = new StringRequest(
                 Request.Method.GET,
